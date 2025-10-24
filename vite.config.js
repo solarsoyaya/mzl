47d1ae3e13663febfa3f1b41ddbd9069d8bbcd14
lv weiquan <2504563338@qq.com>
2025-10-24 11:41:33 +0800

    修复线上部署CSS样式丢失问题

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import { resolve } from 'path'
 export default defineConfig({
   plugins: [vue()],
   base: '/',
+  css: {
+    postcss: './postcss.config.js',
+  },
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
